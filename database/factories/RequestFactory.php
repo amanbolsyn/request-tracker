f881c8fc9a6dc7d4e32099548f0b166234734e96
Aman Bolsyn <nurgozhiyevaman@gmail.com>
2025-12-28 03:19:48 +0500

    feat: added db migrations and factory

---
+++ database/factories/RequestFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class RequestFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => fake()->sentence(5),
+            'body' => fake()->paragraph(),
+            'status' => fake()->randomElement(['new','in progress','completed','rejected']),
+            'category' => fake()->randomElement(['incident', 'service request', 'problem', 'change', 'request for information']),
+            'prioraty' => fake()->randomElement(['critical', 'medium', 'low']),
+        ];
+    }
+}
---
+++ database/migrations/2025_12_27_215530_create_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->string('title');
+            $table->text('body');
+            $table->enum('status', ['new','in progress','completed','rejected']);
+            $table->enum('category', ['incident', 'service request', 'problem', 'change', 'request for information']);
+            $table->enum('prioraty', ['critical', 'medium', 'low']) ;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
